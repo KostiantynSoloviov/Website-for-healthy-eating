417716bb53425d208722a76e3ccdc6af1f6203ad
Kostiantyn <zeleniy.ejik@gnail.com>
2022-03-29 02:17:11 +0300

    website for healthy eating

---
+++ js/script.js
@@ -0,0 +1,31 @@
+import tabs from './modules/tabs';
+import modal from './modules/modal';
+import calc from './modules/calc';
+import cards from './modules/cards';
+import form from './modules/form';
+import slider from './modules/slider';
+import timer from './modules/timer';
+import {openModal} from './modules/modal';
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    const modalTimerId = setTimeout (() => openModal('.modal', modalTimerId), 50000);
+    
+    tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
+    modal('[data-modal]', '.modal', modalTimerId);
+    calc();
+    cards();
+    form('form', modalTimerId);
+    timer('.timer', '2022-04-7');
+    slider({
+        container: '.offer__slider',
+        nextArrow: '.offer__slider-next',
+        slide: '.offer__slide',
+        prevArrow: '.offer__slider-prev',
+        totalCounter: '#total',
+        currentCounter: '#current',
+        wrapper: '.offer__slider-wrapper',
+        field: '.offer__slider-inner'
+    });
+    
+});
